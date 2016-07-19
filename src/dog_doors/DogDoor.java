496a30b906b1d66a114899c332efdf25c50abb71
ShAN <shan.prog@gmail.com>
2016-07-19 11:01:30 +0900

    Собачья дверь Автоматическое закрывание двери

--- src/dog_doors/DogDoor.java
+++ src/dog_doors/DogDoor.java
@@ -1,5 +1,8 @@
 package dog_doors;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class DogDoor {
 
     private boolean open;
@@ -11,6 +14,15 @@ public class DogDoor {
     public void open() {
         System.out.println("The dog door opens.");
         open = true;
+
+        final Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                close();
+                timer.cancel();
+            }
+        }, 5000);
     }
 
     public void close() {
--- src/dog_doors/Remote.java
+++ src/dog_doors/Remote.java
@@ -1,8 +1,5 @@
 package dog_doors;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Remote {
 
     private DogDoor door;
@@ -17,15 +14,6 @@ public class Remote {
             door.close();
         } else {
             door.open();
-
-            final Timer timer = new Timer();
-            timer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    door.close();
-                    timer.cancel();
-                }
-            }, 5000);
         }
     }
 }
