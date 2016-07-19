51f5f104d7fa6bd0e2e7a8395c7917a44e834f39
ShAN <shan.prog@gmail.com>
2016-07-19 10:51:39 +0900

    Собачья дверь вторая реализация немного изменения кода

--- src/dog_doors/DogDoorSimulator.java
+++ src/dog_doors/DogDoorSimulator.java
@@ -6,8 +6,21 @@ public class DogDoorSimulator {
         Remote remote = new Remote(door);
         System.out.println("Fido barks to go outside...");
         remote.pressButton();
+
         System.out.println("\nFido has gone outside...");
         System.out.println("\nFido's all done...");
+
+        try {
+            Thread.currentThread().sleep(10000);
+        } catch (InterruptedException ie) {
+
+        }
+
+        System.out.println("... but he's stuck outside!");
+        System.out.println("Fido starts barking....");
+        System.out.println("...so Gina grabs the remote control.");
+        remote.pressButton();
+
         System.out.println("\nFido's back inside...");
 
     }
